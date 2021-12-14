44e561c877c21be4912bd48db41d325c9c198de8
Thamothiran Naturalist <29664661+thamo-nature@users.noreply.github.com>
2021-12-15 01:17:18 +0530

    Create hacker_manifesto.php

---
+++ hacker_manifesto.php
@@ -0,0 +1,180 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Hacker Manifesto</title>
+
+
+<style>
+  .container{
+  background-color:#262626;
+}
+
+
+blockquote {
+    margin-left: auto;
+    font-size: 1.1em;
+    line-height: 1.6em;
+    padding: 20px 20px 20px 40px;
+    border-left: none;
+    text-indent: -19px
+}
+
+section#block_content {
+    min-height: 400px;
+    padding-top: 40px;
+    text-align: left
+}
+
+blockquote p {
+    font-size: 1em
+}
+
+blockquote:before,
+blockquote:after {
+    font-family: Arial, serif;
+    font-size: 2.5em;
+    vertical-align: middle;
+    line-height: 0
+}
+
+blockquote:before {
+    content: open-quote;
+    margin-right: 4px
+}
+
+blockquote:after {
+    content: close-quote;
+    margin-left: 3px
+}
+
+blockquote.blockstyle,
+blockquote.style2 {
+    /* background: #fff;  */
+    font-style: italic
+}
+
+blockquote.blockstyle p,
+blockquote.style2 p {
+    display: inline
+}
+
+blockquote.blockstyle:before,
+blockquote.blockstyle:after {
+    color: #f0715f
+}
+.card{
+  background-color:grey;
+}
+
+</style>
+
+</head>
+<body>
+
+
+<div style="margin-top:30px;color:#A5B6C8" class="container text-center">
+        <blockquote class="blockstyle">Hacker is not someone who just hacks computer,Hacker is one who thinks outside the People,Religion,Society,Love and Life.,he manipulates Politics,Medicine,Religion,Atoms and Molecules,for the betterment of upcoming generation,He seeks TRUTH.</br> -ThamoNature</blockquote>
+  
+</div>
+
+<!-- Page Content -->
+<!-- <div class="container">
+
+  <div class="row"> -->
+
+  
+  <center>
+    <table border=0 cellspacing=0 cellpadding=0>
+      <tr valign="top">
+        <td width="600">
+
+    <h2 align="center">
+      The Hacker Manifesto
+    </h2>
+    <p align="center">
+      by<br>
+      +++The Mentor+++<br>
+      Written January 8, 1986<br>
+    </p>
+    <p align="left">
+	Another one got caught today, it's all over the papers.
+	"Teenager Arrested in Computer Crime Scandal", "Hacker
+        Arrested after Bank Tampering"...<br><br>
+        Damn kids.  They're all alike.<br><br>
+        But did
+	you, in your three-piece psychology and  <span style="color:lightgreen">1950's technobrain</span>,
+	ever take a look behind the eyes of the hacker ?
+        Did you ever wonder what made him tick, what forces shaped him, what may
+	have molded him?<br><br>
+        I am a hacker, enter my world...<br><br>
+       <span style="color:lightgreen"> Mine is a
+	world that begins with school... I'm smarter than most of the
+	other kids, this crap they teach us bores me...</span><br><br>
+        Damn
+	underachiever.  They're all alike.<br><br>
+        I'm in junior high or high
+	school.  I've listened to teachers explain for the fifteenth
+	time how to reduce a fraction.  I understand it.  "No, Ms.
+	Smith, I didn't show my work.  I did it in my head..."<br><br>
+        Damn kid.  Probably copied it.  They're all alike.<br><br>
+        I made a
+	discovery today.  I found a computer.  Wait a second, this is
+	cool.  It does what I want it to.  If it makes a mistake, it's
+	because I screwed it up.  Not because it doesn't like me...  Or
+	feels threatened by me.. Or thinks I'm a smart ass.. Or doesn't
+	like teaching and shouldn't be here...<br><br>
+        Damn kid.  All he does is play games.  They're all alike.<br><br>
+        And then it happened... a
+	door opened to a world... rushing through the phone line like
+	heroin through an addict's veins, an electronic pulse is sent
+	out, a refuge from the day-to-day incompetencies is sought... a
+	board is found.  "This is it... this is where I belong..." I
+	know everyone here... even if I've never met them, never talked
+	to them, may never hear from them again... I know you all...<br><br>
+	Damn kid.  Tying up the phone line again.  They're all
+	alike...<br><br>
+        You bet your ass we're all alike... <span style="color: lightgreen;">we've been
+	spoon-fed baby food at school when we hungered for steak... the
+	bits of meat that you did let slip through were pre-chewed and
+	tasteless.</span>  We've been dominated by sadists, or ignored by the
+	apathetic.  <span style="color:lightblue"> few that had something to teach found us
+	willing pupils, but those few are like drops of water in the
+	desert.</span>
+        <br><br>
+	This is our world now... the world of the electron and the
+	switch, the beauty of the baud.  We make use of a service
+	already existing without paying for what could be dirt-cheap if
+	it wasn't run by profiteering gluttons, and you call us
+	criminals.  <span style="color:lightgreen">We explore... </span>and <span style="color:lightblue">you call us criminals.</span> <span style="color:lightgreen"> We seek
+	after knowledge...</span> and <span style="color:lightblue">you call us criminals.</span> <span style="color:lightgreen"> We exist without
+	skin color, without nationality, without religious bias...</span>  and
+	<span style="color:lightblue">you call us criminals.</span> You build atomic bombs, you wage wars,
+	you murder, cheat, and lie to us and try to make us believe
+	it's for our own good, yet we're the criminals.<br><br>
+	<span style="color: #E56C5B">Yes, I am a criminal.</span> <span style="color:lightblue"> My crime is that of <span style="color:lightgreen">curiosity.</span>  My crime
+	is that of <span style="color:lightgreen">judging people by what they say and think</span>, not what
+	they look like. My crime is that of <span style="color:lightgreen">outsmarting you</span>, something
+	that you will never forgive me for.<br><br></span>
+        I am a hacker, and this is
+	my manifesto.  You may stop this individual, but you can't stop
+	us all...  after all, we're all alike.
+    </p>
+    <br>
+    <br>
+
+        </td>
+      </tr>
+    </table>
+    <p align="center">
+      +++The End+++<br>
+    </p>
+
+
+</body>
+
+</html>
